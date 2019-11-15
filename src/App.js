db74a18e0cc0a126ebef1d6ab46ede80936800c9
Jenesh <jenesh.napit@gmail.com>
2019-11-15 11:44:08 -0500

    Added some lifecycle and css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
-import Ninjas from './Ninjas';	
+import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 class App extends Component {
 	state = {
 		ninjas: [
-			{name: 'Ryu', age: 23, belt: 'Black', id: 1},
-			{name: 'Ken', age: 23, belt: 'Black',  id: 2},
+			{name: 'Ryu', age: 20, belt: 'Black', id: 1},
+			{name: 'Ken', age: 30, belt: 'Black',  id: 2},
 			{name: 'Scorpion', age: 25, belt: 'Black', id: 3}
 		]
+	}
+
+	addNinja = (ninja) => {
+		ninja.id = this.state.ninjas.length + 1;
+		const ninjas = [...this.state.ninjas, ninja];
+		this.setState({
+			ninjas: ninjas
+		})
+	}
+
+	deleteNinja = (id) => {
+		const ninjas = this.state.ninjas.filter(ninja => {
+			return ninja.id !== id
+		})
+		this.setState({ninjas})
+	}
+
+	componentDidMount() {
+		console.log('Component Mounted')
+	}
+
+	componentDidUpdate(prevProp, prevState) {
+		console.log('Component Updated')
+		console.log(prevProp, prevState)
+	}
+
+	componentWillUnmount(deleted) {
+		console.log(deleted)
 	}
 
 	render () {
 		return (
 			<div className="App">
 				<h1>My first React app!</h1>
-				<Ninjas ninjas={this.state.ninjas}/>
+				<Ninjas ninjas={this.state.ninjas} deleteNinja={this.deleteNinja}/>
+				<AddNinja addNinja={this.addNinja} />
 			</div>
 		);
 	}
