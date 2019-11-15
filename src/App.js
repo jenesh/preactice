0127fb7fa6ee191e63a86c2aad0946027162517e
Jenesh <jenesh.napit@gmail.com>
2019-11-14 22:15:36 -0500

    Added a ui component and a container

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Ninjas from './Ninjas';	
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		ninjas: [
+			{name: 'Ryu', age: 23, belt: 'Black', id: 1},
+			{name: 'Ken', age: 23, belt: 'Black',  id: 2},
+			{name: 'Scorpion', age: 25, belt: 'Black', id: 3}
+		]
+	}
+
+	render () {
+		return (
+			<div className="App">
+				<h1>My first React app!</h1>
+				<Ninjas ninjas={this.state.ninjas}/>
+			</div>
+		);
+	}
 }
 
 export default App;
